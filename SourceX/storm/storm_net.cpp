38bba770d71e39e2112256c25c88170c775378a1
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2019-07-27 10:53:56 +0200

    docs + format.

--- SourceX/storm/storm_net.cpp
+++ SourceX/storm/storm_net.cpp
@@ -1,6 +1,6 @@
 #include "devilution.h"
-#include "stubs.h"
 #include "dvlnet/abstract_net.h"
+#include "stubs.h"
 
 namespace dvl {
 
@@ -14,11 +14,11 @@ static std::unique_ptr<net::abstract_net> dvlnet_inst;
  * @param fileinfo Ignore
  */
 int SNetInitializeProvider(
-	unsigned long            provider,    // type of network connection. game sets this to 'loopback' when playing single player.
-	struct _SNETPROGRAMDATA *client_info, // client information.
-	struct _SNETPLAYERDATA  *user_info,   // user information.
-	struct _SNETUIDATA      *ui_info,     // user interface information.
-	struct _SNETVERSIONDATA *fileinfo     // file information.
+    unsigned long provider,               // type of network connection. game sets this to 'loopback' when playing single player.
+    struct _SNETPROGRAMDATA *client_info, // client information.
+    struct _SNETPLAYERDATA *user_info,    // user information.
+    struct _SNETUIDATA *ui_info,          // user interface information.
+    struct _SNETVERSIONDATA *fileinfo     // file information.
 )
 {
 	dvlnet_inst = net::abstract_net::make_net(provider);
@@ -42,18 +42,18 @@ BOOL SNetSendServerChatCommand(const char *command)
 // although the game never sends a direct message to one of the players.
 // It only broadcasts messages to all players.
 BOOL SNetSendMessage(
-	int          playerID,  // player ID.
-	void        *data,      // message data to send.
-	unsigned int databytes) // length of message data in bytes.
+    int playerID,           // player ID.
+    void *data,             // message data to send.
+    unsigned int databytes) // length of message data in bytes.
 {
 	return dvlnet_inst->SNetSendMessage(playerID, data, databytes);
 }
 
 // Receive a direct message from a player on the network.
 BOOL SNetReceiveMessage(
-	int *senderplayerid, // outputs the ID of the player that sent the message.
-	char **data,         // outputs the data sent as an array of bytes.
-	int *databytes       // outputs the number of bytes sent by the player.
+    int *senderplayerid, // outputs the ID of the player that sent the message.
+    char **data,         // outputs the data sent as an array of bytes.
+    int *databytes       // outputs the number of bytes sent by the player.
 )
 {
 	if (!dvlnet_inst->SNetReceiveMessage(senderplayerid, data, databytes)) {
@@ -65,8 +65,8 @@ BOOL SNetReceiveMessage(
 
 // Send a turn to all connected players over the network.
 BOOL SNetSendTurn(
-	char        *data,     // the array of bytes to send.
-	unsigned int databytes // the length of the array to send in bytes.
+    char *data,            // the array of bytes to send.
+    unsigned int databytes // the length of the array to send in bytes.
 )
 {
 	return dvlnet_inst->SNetSendTurn(data, databytes);
@@ -74,11 +74,11 @@ BOOL SNetSendTurn(
 
 // Receive turns from all connected players over the network.
 BOOL SNetReceiveTurns(
-	int    a1,				 // ??
-	int    arraysize,        // number of pointers in the array.
-	char  *arraydata[],      // the array itself.
-	DWORD *arraydatabytes,   // the length of the array in bytes that the pointer points to.
-	DWORD *arrayplayerstatus // array container the status of the players.
+    int a1,                  // ??
+    int arraysize,           // number of pointers in the array.
+    char *arraydata[],       // the array itself.
+    DWORD *arraydatabytes,   // the length of the array in bytes that the pointer points to.
+    DWORD *arrayplayerstatus // array container the status of the players.
 )
 {
 	if (a1 != 0)
@@ -109,34 +109,34 @@ BOOL SNetGetTurnsInTransit(int *turns)
 // Get provider capabilities.
 int SNetGetProviderCaps(struct _SNETCAPS *caps)
 {
-	caps->size 					= 0;        // engine writes only ?!?
-	caps->flags 				= 0;        // unused
-	caps->maxmessagesize 		= 512;      // capped to 512; underflow if < 24
-	caps->maxqueuesize 			= 0;        // unused
-	caps->maxplayers 			= MAX_PLRS; // capped to 4
-	caps->bytessec 				= 1000000;  // bytes per second?
-	caps->latencyms 			= 0;        // unused
-	caps->defaultturnssec 		= 10;       // default turn per second?
-	caps->defaultturnsintransit = 1; 	    // maximum acceptable number of turns in queue?
+	caps->size = 0;                  // engine writes only ?!?
+	caps->flags = 0;                 // unused
+	caps->maxmessagesize = 512;      // capped to 512; underflow if < 24
+	caps->maxqueuesize = 0;          // unused
+	caps->maxplayers = MAX_PLRS;     // capped to 4
+	caps->bytessec = 1000000;        // bytes per second?
+	caps->latencyms = 0;             // unused
+	caps->defaultturnssec = 10;      // default turn per second?
+	caps->defaultturnsintransit = 1; // maximum acceptable number of turns in queue?
 	return 1;
 }
 
 // Register event handler.
 BOOL SNetRegisterEventHandler(
-	int         evtype, // event type.
-	SEVTHANDLER func    // event handler (callback).
+    int evtype,      // event type.
+    SEVTHANDLER func // event handler (callback).
 )
 {
-	return dvlnet_inst->SNetRegisterEventHandler(*reinterpret_cast<event_type*>(&evtype), func);
+	return dvlnet_inst->SNetRegisterEventHandler(*reinterpret_cast<event_type *>(&evtype), func);
 }
 
 // Unregister event handler.
 BOOL SNetUnregisterEventHandler(
-	int         evtype, // event type.
-	SEVTHANDLER func    // event handler (callback).
+    int evtype,      // event type.
+    SEVTHANDLER func // event handler (callback).
 )
 {
-	return dvlnet_inst->SNetUnregisterEventHandler(*reinterpret_cast<event_type*>(&evtype), func);
+	return dvlnet_inst->SNetUnregisterEventHandler(*reinterpret_cast<event_type *>(&evtype), func);
 }
 
 // Get game information.
@@ -148,16 +148,16 @@ BOOL SNetGetGameInfo(int type, void *dst, unsigned int length, unsigned int *byt
 
 // Called by engine for single player, called by UI for multi player.
 BOOL SNetCreateGame(
-	const char *pszGameName,
-	const char *pszGamePassword,   // game password set by game creator.
-	const char *pszGameStatString,
-    DWORD 		dwGameType,
-    char 	   *GameTemplateData,  // points to the game template data in bytes.
-    int 		GameTemplateSize,  // size of the game template data in bytes.
-    int 		playerCount,
-    char 	   *creatorName,
-    char 	   *a11,
-    int        *playerID           // outputs a generated player ID for the user that created the game.
+    const char *pszGameName,       // unused.
+    const char *pszGamePassword,   // game password set by game creator.
+    const char *pszGameStatString, // unused.
+    DWORD dwGameType,              // game type.
+    char *GameTemplateData,        // points to the game template data in bytes.
+    int GameTemplateSize,          // size of the game template data in bytes.
+    int playerCount,               // max total number of players allowed in the game.
+    char *creatorName,             // unused.
+    char *a11,                     // unused.
+    int *playerID                  // outputs a generated player ID for the user that created the game.
 )
 {
 	if (GameTemplateSize != 8)
@@ -173,12 +173,12 @@ BOOL SNetCreateGame(
 
 // Join an existing game.
 BOOL SNetJoinGame(
-	int   id,
-	char *pszGameName,     // used as IP address.
-	char *pszGamePassword, // game password.
-	char *playerName,      // name of the player that is joining the game.
-	char *userStats,
-	int  *playerID         // outputs your player ID.
+    int id,
+    char *pszGameName,     // used as IP address.
+    char *pszGamePassword, // game password.
+    char *playerName,      // name of the player that is joining the game.
+    char *userStats,
+    int *playerID // outputs your player ID.
 )
 {
 	return (*playerID = dvlnet_inst->join(pszGameName, pszGamePassword)) != -1; // -1 indicates error.
@@ -197,9 +197,8 @@ BOOLEAN SNetSetBasePlayer(int) // engine calls this only once with argument 1.
 
 // Drop/disconnect a player from an established game.
 BOOL SNetDropPlayer(
-	int   playerid, // player ID to drop.
-	DWORD flags
-)
+    int playerid, // player ID to drop.
+    DWORD flags)
 {
 	return dvlnet_inst->SNetDropPlayer(playerid, flags);
 }
@@ -209,5 +208,4 @@ BOOL SNetPerformUpgrade(DWORD *upgradestatus)
 {
 	UNIMPLEMENTED();
 }
-
 }
